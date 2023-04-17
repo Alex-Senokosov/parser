174e7bd6d9f4554c24de4a2d25a4a5f46c892464
alexsenokosov <alexsenokosov666@gmail.com>
2023-04-17 20:13:06 +0300

    ls-2

--- parsers_lessons/Lesson_2/lesson_2.py
+++ parsers_lessons/Lesson_2/lesson_2.py
@@ -11,10 +11,10 @@ import csv
 
 # url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
 #
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
-}
+# headers = {
+#     "Accept": "*/*"
+#     "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
+# }
 #
 # req = requests.get(url, headers=headers)
 # src = req.text
@@ -53,7 +53,7 @@ for category_name, category_href in all_categories.items():
         if item in category_name:
             category_name = category_name.replace(item, "_")
 
-    req = requests.get(url=category_href, headers=headers)
+    req = requests.get(url=category_href)
     src = req.text
 
     with open(f"data/{count}_{category_name}.html", "w") as file:
